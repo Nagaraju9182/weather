e47da58d02cb2e62eb8fff0702706a3a9d223de4
Nagaraju9182 <nagraju.fresher@gmail.com>
2024-07-01 13:57:32 +0530

    added the file

--- slider.js
+++ slider.js
@@ -1,7 +1,42 @@
+// let thumbnails = document.getElementsByClassName("thumbnail");
+// let slider = document.getElementById("slider");
+// let buttonRight = document.getElementById("slide-right");
+// let buttonleft = document.getElementById("slide-left");
+
+// buttonLeft.addEventListener("click", () => {
+//     slider.scrollLeft -= 125;
+// });
+// buttonRight.addEventListener("click", () => {
+//     slider.scrollLeft += 125;
+// });
+
+// const maxScrollLeft =slider.scrollWidth - slider.clientWidth;
+
+
+// function autoplay(){
+//     if(slider.scrollLeft > (maxScrollLeft - 1)){
+//         slider.scrollLeft -= 125;
+//     }else{
+//         slider.scrollLeft += 1;
+// }
+// }
+
+// let play = setInterval(autoplay, 50);
+
+// for (let i =0; i<thumnails.length; i++){
+//     thumnails[i].addEventListener("mouseover", () => {
+//         clearInterval(play)
+//     })
+//     thumnails[i].addEventListener("mouseout", () => {
+//         return play = setInterval(autoplay,50);
+//     })
+// }
+
+
 let thumbnails = document.getElementsByClassName("thumbnail");
 let slider = document.getElementById("slider");
 let buttonRight = document.getElementById("slide-right");
-let buttonleft = document.getElementById("slide-left");
+let buttonLeft = document.getElementById("slide-left");
 
 buttonLeft.addEventListener("click", () => {
     slider.scrollLeft -= 125;
@@ -10,24 +45,23 @@ buttonRight.addEventListener("click", () => {
     slider.scrollLeft += 125;
 });
 
-const maxScrollLeft =slider.scrollWidth - slider.clientWidth;
+const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
 
-
-function autoplay(){
-    if(slider.scrollLeft > (maxScrollLeft - 1)){
+function autoplay() {
+    if (slider.scrollLeft > (maxScrollLeft - 1)) {
         slider.scrollLeft -= 125;
-    }else{
+    } else {
         slider.scrollLeft += 1;
-}
+    }
 }
 
 let play = setInterval(autoplay, 50);
 
-for (let i =0; i<thumnails.length; i++){
-    thumnails[i].addEventListener("mouseover", () => {
-        clearInterval(play)
-    })
-    thumnails[i].addEventListener("mouseout", () => {
-        return play = setInterval(autoplay,50);
-    })
+for (let i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].addEventListener("mouseover", () => {
+        clearInterval(play);
+    });
+    thumbnails[i].addEventListener("mouseout", () => {
+        play = setInterval(autoplay, 50);
+    });
 }
